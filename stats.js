8e42e2d4c91ca2459764c0f4d9c324d0926f7eb1
joaopedro <joaoribeiroc3po@gmail.com>
2023-12-12 20:57:35 -0300

    new commit

--- stats.js
+++ stats.js
@@ -1,25 +1,31 @@
+
 const os = require('os')
 
+//
+setInterval(() => {
+
+    const { frememe,totalmem } = os
+
+    
+    const total = parseInt(totalmem() / 1024 / 1024)
+
+    const mem = parseInt(freemem() /1024 /1024 )
+
+    //criando a porcentagem
+    const percents = parseInt((mem/total) * 100)
+
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    }
+
+    console.clear()
+    console.log(" === PC STATS MEMORY ===")
+    console.table(stats)
 
 
-const { freemem, totalmem } = os
+},1000) 
 
-const total = parseInt(totalmem() / 1024 / 1024)
-
-const mem = parseInt(freemem() /1024 /1024 )
-
-//criando a porcentagem
-const percents = parseInt((mem/total) * 100)
-
-
-
-const stats = {
-    free: `${mem} MB`,
-    total: `${total} MB`,
-    usage: `${percents}%`
-
-}
-console.log(" === PC STATS MEMORY ===")
-console.table(stats)
 
 
