3c8d1036955806261502a87882ab9baa2a04e178
joaopedro <joaoribeiroc3po@gmail.com>
2023-12-12 19:07:53 -0300

    add constants : mem,total,percets

--- stats.js
+++ stats.js
@@ -1,5 +1,15 @@
 const os = require('os')
 
-console.log(os.platform())
 
-const { freemem,total } = os
+
+const { freemem, totalmem } = os
+
+const total = parseInt(totalmem() / 1024 / 1024)
+
+const mem = parseInt(freemem() /1024 /1024 )
+
+//criando a porcentagem
+const percents = parseInt((mem/total) * 100)
+
+console.log (mem,total,percents)
+
